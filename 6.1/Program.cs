da1db6ba768b3251174a9ad69d5e1a92c83219a9
SalivonDaniil <mrtrololo457@gmail.com>
2021-11-28 18:41:32 +0200

    first commit

---
+++ 6.1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+
+namespace _6._1.example
+{
+
+    abstract class AbstractHandler
+    {
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Edit();
+
+    }
+    class Text : AbstractHandler
+    {
+        string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        public virtual string Extension
+        {
+            get
+            {
+                return ".txt";
+            }
+        }
+
+        public override void Open()
+        {
+            Console.Write("Введіть назву файлу: ");
+            string name = Console.ReadLine();
+            string path = desktop + "/" + name + Extension;
+            Console.WriteLine("Текст у файлі: ");
+            string[] read = File.ReadAllLines(path);
+            foreach (string s in read)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public override void Create()
+        {
+            Console.Write("Введіть назву файлу: ");
+            string name = Console.ReadLine();
+            string path = desktop + "/" + name + Extension;
+            Console.WriteLine("Файл створено за шляхом: " + path);
+        }
+        public override void Edit()
+        {
+            Console.Write("Введіть назву файлу: ");
+            string name = Console.ReadLine();
+            string path = desktop + "/" + name + Extension;
+            string text;
+            Console.WriteLine("Текст у файлі:");
+            string[] read = File.ReadAllLines(path);
+            foreach (string s in read)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+            Console.Write("Введіть текст який потрібно замінити: ");
+            string oldText = Console.ReadLine();
+            Console.Write("Новий текст: ");
+            string newText = Console.ReadLine();
+
+            text = File.ReadAllText(path);
+            text = text.Replace(oldText, newText);
+            File.WriteAllText(path, text);
+        }
+    }
+    class Word : Text
+    {
+        public override string Extension
+        {
+            get
+            {
+                return ".docx";
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("1 - txt");
+            Console.WriteLine("2 - docx");
+            Console.Write("Виберіть формат документу: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Text act;
+
+            
+            if (num1 == 1)
+            {
+                act = new Text();
+            }
+            else if (num1 == 2)
+            {
+                act = new Word();
+            }
+            else
+            {
+                throw new Exception("Введіть 1 або 2");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("1 - Відкрити");
+            Console.WriteLine("2 - Створити");
+            Console.WriteLine("3 - Редагувати");
+            Console.Write("Виберіть дію з документом: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num2 == 1)
+            {
+                act.Open();
+            }
+            else if (num2 == 2)
+            {
+                act.Create();
+            }
+            else if (num2 == 3)
+            {
+                act.Edit();
+            }
+            else
+            {
+                throw new Exception("Введіть 1, 2 або 3");
+            }
+        }
+    }
+}
---
+++ 6.3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _6._3
+{
+    interface IPoints
+    {
+        
+        void Check(int points);
+    }
+    abstract class Employee
+    {
+        public string Name;
+        public string Position;
+        public abstract void GetInfo();
+        public Employee(string name, string position)
+        {
+            Name = name;
+            Position = position;
+        }
+    }
+    class Worker : Employee, IPoints
+    {
+        private string _result;
+        public string result
+        {
+            get
+            {
+                return _result;
+            }
+             set
+            {
+                this._result = value;
+            }
+        }
+        public Worker(string name, string position) : base(name, position)
+        {
+
+        }
+        public void Check(int points)
+        {
+            if(points > 50)
+            {
+                result = "Перевірка пройдена";
+            }
+            else
+            {
+                result = "Перевірка не пройдена";
+            }
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Працівник №2\nІм'я: {Name}\nПосада: {Position}\nРезультат: {result}");
+        }
+
+    }
+    class Examinator : Employee
+    {
+        public Examinator(string name, string position) : base(name, position)
+        {
+            
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Працівник №1\nІм'я: {Name}\nПосада: {Position}"); 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Examinator examinator = new Examinator("Дмитро", "Екзамінатор");
+            examinator.GetInfo();
+
+            Console.WriteLine();
+
+            Worker worker = new Worker("Володимир", "Системний адміністратор");
+            worker.Check(60);
+            worker.GetInfo();
+        }
+    }
+}
